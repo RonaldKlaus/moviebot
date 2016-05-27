c02c71b9d39536749b890c42af16e273bef92117
Ronald Klaus <ronald@moviepilot.de>
2016-05-27 11:28:42 +0200

    Add parser

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 
+// IMPORTANT!!!
+var bodyParser = require('body-parser')
+app.use(bodyParser.json());
+
 app.get("/hello", function(request, response) {
   console.log(request)
   response.send("world");
@@ -15,13 +19,14 @@ app.get('/webhook/', function (req, res) {
 });
 
 // recive messages and resent
-app.post('/webhook/', function (req, res) {
+app.post('/webhook', function (req, res) {
   messaging_events = req.body.entry[0].messaging;
   for (i = 0; i < messaging_events.length; i++) {
     event = req.body.entry[0].messaging[i];
     sender = event.sender.id;
     if (event.message && event.message.text) {
       text = event.message.text;
+      console.log(text)
       sendTextMessage(sender, "ECHO: "+ text.substring(0, 200));
     }
   }
