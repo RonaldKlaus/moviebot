a37eec57191669e3e8a23aab9c2e9bd92edcb5a8
Ronald Klaus <ronald@moviepilot.de>
2016-05-27 10:49:24 +0200

    Adding validation and remove code

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
 var express = require('express');
 var app = express();
 
+validationToken = 'psst_this_top_secret'
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
+// athentication
+app.get('/webhook', function (req, res) {
+  if (req.query['hub.verify_token'] === validationToken) {
+    res.send(req.query['hub.challenge']);
+  }
+  res.send('Error, wrong validation token');
+})
 
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+// app.use(express.static(__dirname + '/public'));
+
+// // views is directory for all template files
+// app.set('views', __dirname + '/views');
+// app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+// app.listen(app.get('port'), function() {
+//   console.log('Node app is running on port', app.get('port'));
+// });
 
 
