a0b8a01126bd521c73b485c49bd65747d3a3af60
Ronald Klaus <ronald@moviepilot.de>
2016-05-27 11:25:08 +0200

    Add message handling

--- index.js
+++ index.js
@@ -6,11 +6,48 @@ app.get("/hello", function(request, response) {
   response.send("world");
 });
 
+// Authentication
 app.get('/webhook/', function (req, res) {
   if (req.query['hub.verify_token'] === 'psst_this_top_secret') {
     res.send(req.query['hub.challenge']);
   }
   res.send('Error, wrong validation token');
 });
+
+// recive messages and resent
+app.post('/webhook/', function (req, res) {
+  messaging_events = req.body.entry[0].messaging;
+  for (i = 0; i < messaging_events.length; i++) {
+    event = req.body.entry[0].messaging[i];
+    sender = event.sender.id;
+    if (event.message && event.message.text) {
+      text = event.message.text;
+      sendTextMessage(sender, "ECHO: "+ text.substring(0, 200));
+    }
+  }
+  res.sendStatus(200);
+});
+
+// for message handling
+function sendTextMessage(sender, text) {
+  messageData = {
+    text:text
+  }
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token:token},
+    method: 'POST',
+    json: {
+      recipient: {id:sender},
+      message: messageData,
+    }
+  }, function(error, response, body) {
+    if (error) {
+      console.log('Error sending message: ', error);
+    } else if (response.body.error) {
+      console.log('Error: ', response.body.error);
+    }
+  });
+}
 
 app.listen(process.env.PORT)
