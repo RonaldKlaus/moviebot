d8acc99760323a1c98da04cf9699469b5d9c6694
Ronald Klaus <ronald@moviepilot.de>
2016-05-27 11:00:39 +0200

    Bugfixing

--- index.js
+++ index.js
@@ -1,29 +1,10 @@
 var express = require('express');
 var app = express();
 
-validationToken = 'psst_this_top_secret'
-app.set('port', (process.env.PORT || 5000));
-
-// athentication
-app.get('/webhook/', function (req, res) {
-  if (req.query['hub.verify_token'] === validationToken) {
-    res.send(req.query['hub.challenge']);
-  }
-  res.send('Error, wrong validation token');
+app.get("/", function(request, response) {
+  "HELLO !";
 })
 
-// app.use(express.static(__dirname + '/public'));
-
-// // views is directory for all template files
-// app.set('views', __dirname + '/views');
-// app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index');
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
-
-// app.listen(app.get('port'), function() {
-//   console.log('Node app is running on port', app.get('port'));
-// });
-
-
